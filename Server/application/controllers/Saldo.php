09ccc80b2f09c955a778119cdc7bfb83541b4da7
mahardikaakbar <mahardikaakbarwijaya@gmail.com>
2023-01-07 20:31:13 +0700

    Dika : Menonaktifkan Token

--- Server/application/controllers/Saldo.php
+++ Server/application/controllers/Saldo.php
@@ -14,7 +14,7 @@ class Saldo extends Server{
             $this->response([
                 'status' => true,
                 'message' => 'Berhasil',
-                'Total Saldo ' => $hasil
+                'Total Saldo' => $hasil
         ],200);
         }else{
             $this->response([
