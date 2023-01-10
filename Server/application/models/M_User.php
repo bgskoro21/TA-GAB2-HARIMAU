9bde9c6b809247dfae3445d10d95b73b1d6920b4
mahardikaakbar <mahardikaakbarwijaya@gmail.com>
2023-01-10 14:22:22 +0700

    Dika : Memperbaiki Saldo

--- Server/application/models/M_User.php
+++ Server/application/models/M_User.php
@@ -191,7 +191,7 @@ class M_User extends CI_Model{
         }else{
             $hasil = 0;
         }
-        return $query;
+        return $hasil;
     }
 
     function forgotpassword($email,$password,$is_active){
@@ -206,6 +206,8 @@ class M_User extends CI_Model{
         } else {
             $hasil = 0;
         }
+
+        return $hasil;
     }
     
     public function aktivasi_akun($is_active,$email){
